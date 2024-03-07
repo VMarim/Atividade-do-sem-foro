8305ced85aa9e74158eebe86c5735edb7edd2fb6
Alesy Gabriel <alesygabriel3@gmail.com>
2024-03-06 23:30:27 -0300

    arrumei semaforo

--- script.js
+++ script.js
@@ -3,44 +3,43 @@ var emergencyMode = false;
 
 function changeLights() {
   if (!emergencyMode) {
-    var redLight = document.querySelector('.red');
-    var yellowLight = document.querySelector('.yellow');
-    var greenLight = document.querySelector('.green');
+    var redLight = document.getElementById('red');
+    var yellowLight = document.getElementById('yellow');
+    var greenLight = document.getElementById('green');
 
     if (redLight.style.backgroundColor === "red") {
-      redLight.style.backgroundColor = "black";
+      redLight.style.backgroundColor ='rgba(255, 0, 0, 0.377)';
       yellowLight.style.backgroundColor = "yellow";
     } else if (yellowLight.style.backgroundColor === "yellow") {
-      yellowLight.style.backgroundColor = "black";
+      yellowLight.style.backgroundColor = 'rgba(248, 248, 10, 0.381)';
       greenLight.style.backgroundColor = "green";
     } else {
-      greenLight.style.backgroundColor = "black";
+      greenLight.style.backgroundColor = 'rgba(15, 193, 15, 0.359)';
       redLight.style.backgroundColor = "red";
     }
   }
 }
 
-function emergency() {
-  if (!emergencyMode) {
-    emergencyMode = true;
-    clearInterval(intervalId); // Parar a sequência anterior
-    intervalId = setInterval(blinkYellow, 500); // Iniciar o modo de emergência (pisca-amarelo)
+
+function toggleCheck() {
+  if(document.getElementById("myCheckbox").checked === true){
+    clearInterval(intervalId)
+    intervalId = setInterval(changeLights, 2000);
   } else {
-    emergencyMode = false;
-    clearInterval(intervalId); // Parar o modo de emergência
-    intervalId = setInterval(changeLights, 2000); // Reiniciar a sequência normal
+    clearInterval(intervalId)
+    intervalId = setInterval(blinkYellow, 500);
   }
 }
 
 function blinkYellow() {
-    var redLight = document.querySelector('.red');
-    var yellowLight = document.querySelector('.yellow');
-    var greenLight = document.querySelector('.green');
-    greenLight.style.backgroundColor = "black";
-    redLight.style.backgroundColor = "black";
+    var redLight = document.getElementById('red');
+    var yellowLight = document.getElementById('yellow');
+    var greenLight = document.getElementById('green');
+    greenLight.style.backgroundColor = 'rgba(15, 193, 15, 0.359)';
+    redLight.style.backgroundColor = 'rgba(255, 0, 0, 0.377)';
 
   if (yellowLight.style.backgroundColor === "yellow") {
-    yellowLight.style.backgroundColor = "black";
+    yellowLight.style.backgroundColor = 'rgba(248, 248, 10, 0.381)';
   } else {
     yellowLight.style.backgroundColor = "yellow";
   }
